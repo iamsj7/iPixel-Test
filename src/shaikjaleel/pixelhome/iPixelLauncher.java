6589346c44cef711109d6f5ac7793fcfe6ffd84c
Chris <chris@cypheros.co>
2018-12-16 19:11:15 -0500

    PixleHome: Introduce Quickspace Based On LunaLauncher
    * Implemented form CyperOS LunaLauncher
    
    Implements a date/time and weather view on the first page of the
    homescreen similar to Google's Smartspace.
    
    This completely replaces the legacy QSB with our view and uses an open
    source WeatherProvider client to retrieve weather information
    
    Change-Id: Iac9c8099007f790322342e23b5911fc2ecf808ea

--- src/shaikjaleel/pixelhome/iPixelLauncher.java
+++ src/shaikjaleel/pixelhome/iPixelLauncher.java
@@ -21,9 +21,12 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 
+import com.shaikjaleel.pixelhome.quickspace.QuickSpaceView;
+
 import com.android.launcher3.AppInfo;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherCallbacks;
+import com.android.launcher3.R;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -38,16 +41,23 @@ public class iPixelLauncher extends Launcher {
     public class iPixelLauncherCallbacks implements LauncherCallbacks, OnSharedPreferenceChangeListener {
 
         private final iPixelLauncher mLauncher;
+        private QuickSpaceView mQuickSpace;
 
         public iPixelLauncherCallbacks(iPixelLauncher launcher) {
             mLauncher = launcher;
         }
 
         @Override
-        public void onCreate(Bundle savedInstanceState) { }
+        public void onCreate(Bundle savedInstanceState) {
+            mQuickSpace = mLauncher.findViewById(R.id.reserved_container_workspace);
+        }
 
         @Override
-        public void onResume() { }
+        public void onResume() {
+            if (mQuickSpace != null) {
+                mQuickSpace.onResume();
+            }
+        }
 
         @Override
         public void onStart() { }
